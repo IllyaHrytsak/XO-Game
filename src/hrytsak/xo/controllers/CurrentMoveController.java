b65e2bbb8ae8514f64e239427e8d49382b30dd73
iMac <gritsak_web@bigmir.net>
2016-06-23 19:08:13 +0300

    CurrentMoveController - created

---
+++ src/hrytsak/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,42 @@
+package hrytsak.xo.controllers;
+
+import hrytsak.xo.model.Field;
+import hrytsak.xo.model.Figure;
+import hrytsak.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+        int countFigure = 0;
+        for (int y = 0; y < field.getSize(); y++) {
+            countFigure += countFiguresInTheRow(field, y);
+        }
+
+        if (countFigure == field.getSize() * field.getSize()) {
+            return null;
+        }
+
+        if (countFigure %2 == 0) {
+            return Figure.X;
+        }
+
+
+        return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Field field, final int row) {
+        int countFigure = 0;
+        for (int x = 0; x < field.getSize(); x++) {
+            try {
+                if (field.getFigure(new Point(x, row)) != null) {
+                    countFigure++;
+                }
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+        }
+        return countFigure;
+    }
+}
