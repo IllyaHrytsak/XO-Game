6cd32473b0cb577c996e1321247bdbb6ab3245b0
Illya Hrytsak <gritsak_web@bigmir.net>
2016-09-03 12:44:46 +0300

    updated CLI

--- src/hrytsak/xo/XOCLI.java
+++ src/hrytsak/xo/XOCLI.java
@@ -12,27 +12,21 @@ import java.util.Scanner;
 public class XOCLI {
 
     public static void main(final String[] args) {
-        System.out.print("Input player name 1: ");
-        final String name1 = inputName();
-        System.out.print("Input player name 2: ");
-        final String name2 = inputName();
+        final String name1 = "Gleb";
+        final String name2 = "Slava";
+
         final Player[] players = new Player[2];
         players[0] = new Player(name1, Figure.X);
         players[1] = new Player(name2, Figure.O);
-        final Game game = new Game(players, new Field(3), "XO");
+
+        final Game gameXO = new Game(players, new Field(3), "XO");
 
         final ConsoleView consoleView = new ConsoleView();
-        consoleView.show(game);
-        while(consoleView.move(game)) {
-            consoleView.show(game);
+        consoleView.show(gameXO);
+        while(consoleView.move(gameXO)) {
+            consoleView.show(gameXO);
         }
     }
 
-    static String inputName() {
-        final Scanner sc = new Scanner(System.in);
-        return sc.next();
-    }
-
-    
 
 }
